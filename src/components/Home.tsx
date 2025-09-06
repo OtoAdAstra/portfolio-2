13646e7ca6cf46dd357be5d6e2b4c9f266fdef06
OtoBourbon <sharvashidze0712@gmail.com>
2025-09-07 00:09:12 +0400

    updated home div

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,18 @@
 import Saturn from "../assets/saturn.png";
+import wave from "../assets/wave.png";
 import "../styles/home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <img src={Saturn} alt="Saturn" className="saturn-image" />
+      <h1
+        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+        Hello!
+        <span>
+          <img src={wave} alt="Wave" className="wave-image" />
+        </span>
+      </h1>
     </div>
   );
 };
