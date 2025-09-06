acce141472690140a95cc8f8b9339ef4347fbe9a
OtoBourbon <sharvashidze0712@gmail.com>
2025-09-06 23:58:25 +0400

    added home section

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import Saturn from "../assets/saturn.png";
+import "../styles/home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <img src={Saturn} alt="Saturn" className="saturn-image" />
+    </div>
+  );
+};
+
+export default Home;
